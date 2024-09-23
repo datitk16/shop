8e9041c5e41740d8b215c2939ff7c7aca68b3666
datnd35 <datitk16@gmail.com>
2024-09-23 14:54:59 +0700

    first commit

---
+++ src/app/remote-entry/entry.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule],
+  selector: 'app-shop-entry',
+  template: `
+  <h1>Tao là shop</h1>
+  `,
+})
+export class RemoteEntryComponent { }
